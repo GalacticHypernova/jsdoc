26c31a22203ecda4a7a339259b05f866fcb5a128
Michael Brevard <yonshi29@gmail.com>
2024-01-25 15:54:15 +0200

    types: add JSDoc

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,9 @@
 export declare const files: Map<string, string>;
+/**
+* A utility for extracting the JSDoc annotations from a member in the given module, if it exists.
+  @param modulePath - The path to the module containing the function.
+  @param member - The name of the member to extract the JSDoc annotations from.
+  @returns The JSDoc annotations for the given function, if it exists.
+  @throws {Error} if something went wrong
+*/
 export declare function extractJSDoc(modulePath: string, member: string): RegExpMatchArray | null | undefined;
