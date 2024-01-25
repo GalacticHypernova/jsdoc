fb4093393c7ee8fd72b82b142e7a538d27802815
Michael Brevard <yonshi29@gmail.com>
2024-01-25 15:58:20 +0200

    test: prepare for additional utils

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,24 +2,26 @@ import { expect, it, describe } from "vitest";
 import { extractJSDoc } from "../src";
 
 describe("unjsdoc", () => {
-  it("should extract jsdoc", () => {
-    const jsdoc = extractJSDoc("../playground", "hasJSDoc");
-    expect(jsdoc[0]).toMatchSnapshot(`
-        /**
-         * Test function with JSDoc
-         * @param test
-         * @param test2
-         * @returns {number}
-         */`);
-  });
-  it("should return null for non-existent jsdoc", () => {
-    const jsdoc = extractJSDoc("../playground", "noJSDoc");
-    // eslint-disable-next-line unicorn/no-null
-    expect(jsdoc).toEqual(null);
-  });
-  it("should return null for non-existent function", () => {
-    const jsdoc = extractJSDoc("../playground", "noFunction");
-    // eslint-disable-next-line unicorn/no-null
-    expect(jsdoc).toEqual(null);
+  decribe("extractJSDoc", () => {
+    it("should extract jsdoc", () => {
+      const jsdoc = extractJSDoc("../playground", "hasJSDoc");
+      expect(jsdoc[0]).toMatchSnapshot(`
+          /**
+           * Test function with JSDoc
+           * @param test
+           * @param test2
+           * @returns {number}
+           */`);
+    });
+    it("should return null for non-existent jsdoc", () => {
+      const jsdoc = extractJSDoc("../playground", "noJSDoc");
+      // eslint-disable-next-line unicorn/no-null
+      expect(jsdoc).toEqual(null);
+    });
+    it("should return null for non-existent function", () => {
+      const jsdoc = extractJSDoc("../playground", "noFunction");
+      // eslint-disable-next-line unicorn/no-null
+      expect(jsdoc).toEqual(null);
+    });
   });
 });
